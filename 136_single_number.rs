82bfe84a4a13e4c7aaf8ed0ff2715b53f5a0e2a9
GARROUSTE Adrien <garrousteadrien@gmail.com>
2022-03-04 16:22:40 +0100

    Add 136

---
+++ 136_single_number.rs
@@ -0,0 +1,18 @@
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn single_number(nums: Vec<i32>) -> i32 {
+        if nums.len() == 1 {
+            return nums[0];
+        }
+        let mut occurs = HashMap::new();
+        
+        for &v in nums.iter() {
+            let count = occurs.entry(v).or_insert(0);
+            *count += 1;
+        }
+        *occurs.iter()
+            .filter(|t| *t.1 == 1)
+            .collect::<Vec<_>>()[0].0
+    }
+}
