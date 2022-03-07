bd9a8db4ed0023bb3c7f9c73422ac4ef88e567e8
Adrien <garrousteadrien@gmail.com>
2022-03-07 23:42:07 +0100

    Add 118 & 175

---
+++ 118_pascals_triangle.rs
@@ -0,0 +1,19 @@
+impl Solution {
+    pub fn generate(num_rows: i32) -> Vec<Vec<i32>> {
+        let mut result = vec![vec![1]];
+        
+        (1..num_rows as usize).for_each(|i| {
+            result.push(vec![]);
+            (0..i + 1 as usize).for_each(|j| {
+                if j == 0 || j == i {
+                    result[i].push(1); 
+                } else {
+                    let x = result[i - 1][j - 1] + result[i - 1][j];
+                    result[i].push(x);   
+                }
+            });   
+        });
+        
+        result
+    }
+}
