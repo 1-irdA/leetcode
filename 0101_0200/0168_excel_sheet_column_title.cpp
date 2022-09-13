9f4fce910d3a00bc192dbc0263d0430c192272eb
Adrien <39885692+1-irdA@users.noreply.github.com>
2022-09-13 08:47:55 +0200

    Add 0168

---
+++ 0101_0200/0168_excel_sheet_column_title.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+        string res;
+        char c;
+        
+        while (columnNumber > 0) {
+            c = 'A' + (columnNumber - 1) % 26;
+            res = c + res;
+            columnNumber = (columnNumber - 1) / 26;
+        }
+        return res;
+    }
+};
